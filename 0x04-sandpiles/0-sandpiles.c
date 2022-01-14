2fc6af5af4c388f473d273bc661b658088c82808
Felipe Prado <jfpc11@misena.edu.co>
2022-01-13 23:41:49 -0500

    adding the correct function betty not yet correct

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -8,7 +8,7 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, end_process = 1, end_all = 0;
+	int i, j, end_process = 1, all_sum = 0, count = 0;
 
 	for (i = 0; i < 3; i++)
 	{
@@ -26,84 +26,54 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		{
 			for (j = 0; j < 3; j++)
 			{
+				grid2[i][j] = grid1[i][j];
 				if (grid1[i][j] >= 4)
-				{
-					if (i == 0 && j == 0)
-					{
-						grid1[0][1] += 1;
-						grid1[1][0] += 1;
-						grid1[0][0] -= 4;
-						end_process = 1;
-					}
-					if (i == 0 && j == 1)
-					{
-						grid1[0][0] += 1;
-						grid1[0][2] += 1;
-						grid1[1][1] += 1;
-						grid1[0][1] -= 4;
-						end_process = 1;
-					}
-					if (i == 0 && j == 2)
-					{
-						grid1[0][1] += 1;
-						grid1[1][2] += 1;
-						grid1[0][2] -= 4;
-						end_process = 1;
-					}
-					if (i == 1 && j == 0)
-					{
-						grid1[0][0] += 1;
-						grid1[1][1] += 1;
-						grid1[2][0] += 1;
-						grid1[1][0] -= 4;
-						end_process = 1;
-					}
-					if (i == 1 && j == 1)
-					{
-						grid1[0][1] += 1;
-						grid1[1][0] += 1;
-						grid1[1][2] += 1;
-						grid1[2][1] += 1;
-						grid1[1][1] -= 4;
-						end_process = 1;
-					}
-					if (i == 1 && j == 2)
-					{
-						grid1[0][2] += 1;
-						grid1[1][1] += 1;
-						grid1[2][2] += 1;
-						grid1[1][2] -= 4;
-						end_process = 1;
-					}
-					if (i == 2 && j == 0)
-					{
-						grid1[1][0] += 1;
-						grid1[2][1] += 1;
-						grid1[2][0] -= 4;
-						end_process = 1;
-					}
-					if (i == 2 && j == 1)
-					{
-						grid1[2][0] += 1;
-						grid1[1][1] += 1;
-						grid1[2][2] += 1;
-						grid1[2][1] -= 4;
-						end_process = 1;
-					}
-					if (i == 2 && j == 2)
-					{
-						grid1[1][2] += 1;
-						grid1[2][1] += 1;
-						grid1[2][2] -= 4;
-						end_process = 1;
-					}
-				}
+					all_sum += 1;
 			}
 		}
-		if (end_process == 1 && end_all == 1)
+
+		if (all_sum > 0)
 		{
 			printf("=\n");
 			print_grid_topple(grid1);
+		}
+
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] >= 4 && grid2[i][j] >= 4 && count < all_sum)
+				{
+					if (i - 1 >= 0)
+					{
+						grid1[i - 1][j] += 1;
+						if (grid1[i - 1][j] >= 4)
+							end_process = 1;
+					}
+					if (i + 1 < 3)
+					{
+						grid1[i + 1][j] += 1;
+						if (grid1[i + 1][j] >= 4)
+							end_process = 1;
+					}
+					if (j - 1 >= 0)
+					{
+						grid1[i][j - 1] += 1;
+						if (grid1[i][j - 1] >= 4)
+							end_process = 1;
+					}
+					if (j + 1 < 3)
+					{
+						grid1[i][j + 1] += 1;
+						if (grid1[i][j + 1] >= 4)
+							end_process = 1;
+					}
+					grid1[i][j] -= 4;
+					if (grid1[i][j] >= 4)
+						end_process = 1;
+					count += 1;
+				}
+			}
 		}
 	}
 }
@@ -113,19 +83,62 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
  * print_grid_topple - to print the grid
  * @grid: the grid to print
  */
-
 void print_grid_topple(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
+
+
+// /**
+//  * refactor_spaces - refactoring ifs
+//  * @grid1: the grid to be changed
+//  * @i: iterator for the loop
+//  * @j: iterator for the inner loop
+//  * @end_process: the state if the proccess end or not
+//  *
+//  * Return: the end_proccess state
+//  */
+
+// int refactor_spaces(int grid1[3][3], int i, int j, int end_process)
+// {
+// 	if (i - 1 >= 0)
+// 	{
+// 		grid1[i - 1][j] += 1;
+// 		if (grid1[i - 1][j] >= 4)
+// 			end_process = 1;
+// 	}
+// 	if (i + 1 < 3)
+// 	{
+// 		grid1[i + 1][j] += 1;
+// 		if (grid1[i + 1][j] >= 4)
+// 			end_process = 1;
+// 	}
+// 	if (j - 1 >= 0)
+// 	{
+// 		grid1[i][j - 1] += 1;
+// 		if (grid1[i][j - 1] >= 4)
+// 			end_process = 1;
+// 	}
+// 	if (j + 1 < 3)
+// 	{
+// 		grid1[i][j + 1] += 1;
+// 		if (grid1[i][j + 1] >= 4)
+// 			end_process = 1;
+// 	}
+// 	grid1[i][j] -= 4;
+// 	if (grid1[i][j] >= 4)
+// 		end_process = 1;
+
+// 	return (end_process);
+// }
