2554c06bf02542c73671fb108582db0d03ad48c2
Felipe Prado <jfpc11@misena.edu.co>
2022-02-17 21:07:42 -0500

    making the last

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,18 +7,19 @@ def validUTF8(data):
     """returns true if it is utf-8 otherwise false
     """
     count = 0
-    for c in data:
+    for num in data:
         if count == 0:
-            if (c >> 5) == 0b110:
+            if (num >> 5) == 0b110:
                 count = 1
-            elif (c >> 4) == 0b1110:
+            elif (num >> 4) == 0b1110:
                 count = 2
-            elif (c >> 3) == 0b11110:
+            elif (num >> 3) == 0b11110:
                 count = 3
-            elif (c >> 7):
+            elif (num >> 7):
                 return False
+
         else:
-            if (c >> 6) != 0b10:
+            if (num >> 6) != 0b10:
                 return False
             count -= 1
     return count == 0
