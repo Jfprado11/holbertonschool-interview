f59cc1658cbfef45a1f6b0a9e599598607765090
Felipe Prado <jfpc11@misena.edu.co>
2022-03-24 21:21:40 -0500

    retriving the trees

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,69 @@
+#include "binary_trees.h"
+/**
+ * sorted_array_to_avl - a sorted array into a tree avl
+ * @array: the array to convert into the tree
+ * @size: the size of the array
+ *
+ * Return: a tree avl
+ */
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *root = NULL;
+
+	root = helper(array, 0, size - 1, NULL);
+	return (root);
+}
+
+/**
+ * helper - a sorted array into a tree avl
+ * @arr: the array to convert into the tree
+ * @end: the size of the array
+ * @start: the start of the array
+ *
+ * Return: a tree avl
+ */
+avl_t *helper(int *arr, size_t start, size_t end, avl_t *previos_node)
+{
+	size_t mid = 0;
+	avl_t *root = NULL;
+
+	if (start >= end)
+	{
+		return (NULL);
+	}
+
+	mid = start + (end - start) / 2;
+	root = new_node(arr[mid], previos_node);
+	if (root == NULL)
+		return NULL;
+
+	// printf("%ld %ld\n", start, end);
+	root->left = helper(arr, start, mid - 1, root);
+	root->right = helper(arr, mid + 1, end, root);
+
+	return (root);
+}
+
+
+/**
+ * new_node - creates a new node
+ * @data: the value of the node
+ * @previos_node: the parent of the node
+ * Return: the node
+ */
+avl_t* new_node(int data, avl_t *previos_node)
+{
+	avl_t *node = NULL;
+
+	node = malloc(sizeof(avl_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = data;
+	node->right = NULL;
+	node->left = NULL;
+	node->parent = previos_node;
+
+	return (node);
+}
