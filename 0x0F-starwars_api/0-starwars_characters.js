a69fa707d181234fb5a25db3aa1fe8b6fdf9261d
Felipe Prado <jfpc11@misena.edu.co>
2022-04-07 19:13:14 -0500

    getting the data as needed

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@ const request = require('request');
 const filmId = process.argv[2];
 const filmUrl = `https://swapi-api.hbtn.io/api/films/${filmId}/`;
 
-function firstInter() {
+function firstInter () {
   return new Promise((resolve, reject) => {
     request.get({ url: filmUrl, json: true }, function (error, response, body) {
       if (!error && response.statusCode === 200) {
@@ -13,13 +13,12 @@ function firstInter() {
   });
 }
 
-function secondInter(urlTofetch) {
+function secondInter (urlTofetch) {
   return new Promise((resolve, reject) => {
     request.get(
       { url: urlTofetch, json: true },
       function (error, response, body) {
         if (!error && response.statusCode === 200) {
-          console.log(body.name);
           resolve(body);
         }
       }
@@ -27,12 +26,12 @@ function secondInter(urlTofetch) {
   });
 }
 
-async function main() {
+async function main () {
   const data = await firstInter();
-  data.characters.forEach(async (element) => {
-    const dataChar = await secondInter(element);
-    // console.log(dataChar.name);
-  });
+  for (const datechar of data.characters) {
+    const datachar = await secondInter(datechar);
+    console.log(datachar.name);
+  }
 }
 
 main();
