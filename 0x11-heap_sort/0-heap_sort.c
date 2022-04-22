b69e416dab7c4ea8ab219a85e2f4a03592144944
Felipe Prado <jfpc11@misena.edu.co>
2022-04-21 23:36:09 -0500

    fixinx betty

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -13,56 +13,38 @@ void swap(int *a, int *b);
 
 void heap_sort(int *array, size_t size)
 {
-    for (int i = size / 2 - 1; i >= 0; i--)
-      heapify(array, size, i, size);
-    // Heap sort
-    for (int i = size - 1; i >= 0; i--) {
-      swap(&array[0], &array[i]);
-	  print_array(array, size);
-      // Heapify root element to get highest element at root again
-      heapify(array, i, 0, size);
-    }
-}
-
-void siftdown(int array[], int root, int last)
-{
-	int largest = root, l = 2 * root + 1, r = 2 * root + 1, temp = 0;
-
-	if (l <= last && array[largest] < array[l])
-		largest = l;
-
-	if (r <= last && array[largest] < array[r])
-		largest  = r;
-
-	if (largest != root)
+	for (int i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i, size);
+	for (int i = size - 1; i >= 0; i--)
 	{
-		temp = array[largest];
-		array[largest] = array[root];
-		array[root] = temp;
-
-		siftdown(array, largest, last);
+		swap(&array[0], &array[i]);
+		print_array(array, size);
+		heapify(array, i, 0, size);
 	}
 }
 
-
-void build_heap_max(int array[], int end)
+/**
+ * swap - swap two values
+ * @a: integer to do swap
+ * @b: to do the swap
+ */
+void swap(int *a, int *b)
 {
-	int i = (end - 1) / 2;
+	int temp = *a;
 
-	while (i >= 0)
-	{
-		siftdown(array, i, end);
-		i--;
-	}
+	*a = *b;
+	*b = temp;
 }
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-  }
-
-void heapify(int arr[], int n, int i, int size) {
+/**
+ * heapify - swap the value acording
+ * @arr: the array
+ * @n: the salt node
+ * @i: the first node
+ * @size: the size of the array
+ */
+void heapify(int arr[], int n, int i, int size)
+{
 	int largest = i;
 	int left = 2 * i + 1;
 	int right = 2 * i + 2;
@@ -73,7 +55,8 @@ void heapify(int arr[], int n, int i, int size) {
 	if (right < n && arr[right] > arr[largest])
 	largest = right;
 
-	if (largest != i) {
+	if (largest != i)
+	{
 	swap(&arr[i], &arr[largest]);
 	print_array(arr, size);
 	heapify(arr, n, largest, size);
