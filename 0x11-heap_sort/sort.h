956e9b5731752212142c60be10bd345b2da26763
Felipe Prado <jfpc11@misena.edu.co>
2022-04-21 23:28:29 -0500

    adding the sort

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+
+#endif /* _SEARCH_H_ */
