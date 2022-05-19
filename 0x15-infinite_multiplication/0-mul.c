4c2e960cb39784a43d7a6021130482bc3101f0d3
Felipe Prado <jfpc11@misena.edu.co>
2022-05-19 12:52:02 -0500

    checking infite number

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,83 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- the main function
+ *@argc: varible counts
+ *@argv: a string
+ * Return: a number in success or failed
+ */
+int main(int argc, char *argv[])
+{
+	int a, check_a, check_b;
+
+	if (argc != 3)
+	{
+		printError();
+		exit(98);
+	}
+	check_a = checkDigit(argv[1]);
+	check_b = checkDigit(argv[2]);
+	if (check_a == 0 || check_b == 0)
+	{
+		printError();
+		exit(98);
+	}
+	a = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d \n", a);
+}
+
+/**
+ * printError- the main function
+ *
+ */
+void printError(void)
+{
+	char *error = "Error";
+	int i = 0;
+
+	for (; i <= 4; i++)
+	{
+		_putchar(error[i]);
+	}
+	_putchar('\n');
+
+}
+
+int checkDigit(char *strI)
+{
+	int i;
+
+	for (i = 0; i < _strlen(strI); i++)
+	{
+		if (strI[i] >= '0' && strI[i] <= '9')
+		{
+			continue;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ *_strlen - Tells the length or size of a string
+ *@s: varible to test the string
+ *
+ *Return: len the length of the string
+ */
+int _strlen(char *s)
+{
+	int lon = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		lon++;
+	}
+	return (lon);
+}
+
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void printError(void);
+int _strlen(char *s);
+int checkDigit(char *strI);
+
+
+#endif
