68f7bed23e027db7cbeffd657d64071be78c75ed
Felipe Prado <jfpc11@misena.edu.co>
2022-05-19 13:03:14 -0500

    i dont knwo

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "multiplication.h"
 /**
  * main- the main function
  *@argc: varible counts
---
+++ 0x15-infinite_multiplication/multiplication.h
@@ -0,0 +1,12 @@
+#ifndef _MULTIPLICATION_H_
+#define _MULTIPLICATION_H_
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void printError(void);
+int _strlen(char *s);
+int checkDigit(char *strI);
+
+
+#endif /* _MULTIPLICATION_H_ */
