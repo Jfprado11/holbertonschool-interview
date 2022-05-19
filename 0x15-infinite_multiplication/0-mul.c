49b103a8c02ba33857ef861f5d79532b8906d4f3
Felipe Prado <jfpc11@misena.edu.co>
2022-05-19 13:10:13 -0500

    checking for erros

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int check_a, check_b;
+	int result, check_a, check_b;
 
 	if (argc != 3)
 	{
@@ -21,7 +21,8 @@ int main(int argc, char *argv[])
 		printError();
 		exit(98);
 	}
-	printf("%d", argv[1][0]);
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
 	return (0);
 }
 
