66a992dd74019564b3c3fc5e180ee7b1009dd5ef
Felipe Prado <jfpc11@misena.edu.co>
2022-06-09 21:01:59 -0500

    fixing betty

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -8,12 +8,12 @@
 
 void merge_sort(int *array, size_t size)
 {
-	merge_sort_res(array, 0, size -1);
+
+	merge_sort_res(array, 0, size - 1);
 }
 
-
 /**
- * mergeSort_res - a merge to start the serach of the array
+ * merge_sort_res - a merge to start the serach of the array
  *@array: the array to do the sorting
  *@left: the idx of the left size of the array
  *@right: the idx of the right size of the array
@@ -35,7 +35,7 @@ void merge_sort_res(int *array, int left, int right)
 }
 
 /**
- * mergeSort_res - a merge to start the serach of the array
+ * merge - a merge to start the serach of the array
  *@array: the array to do the sorting
  *@left: the idx of the left size of the array
  *@middle: the midle of the aray
@@ -43,43 +43,43 @@ void merge_sort_res(int *array, int left, int right)
  */
 void merge(int *array, int left, int middle, int right)
 {
-	int i, j, k;
-    int n1 = middle - left + 1;
-    int n2 = right - middle;
+	int i, j, k, n1 = middle - left + 1, n2 = right - middle;
 	int left_tmp[n1];
 	int right_tmp[n2];
 
-
 	for (i = 0; i < n1; i++)
-        left_tmp[i] = array[left + i];
-    for (j = 0; j < n2; j++)
-	{
-        right_tmp[j] = array[middle + 1 + j];
-	}
+		left_tmp[i] = array[left + i];
+	for (j = 0; j < n2; j++)
+		right_tmp[j] = array[middle + 1 + j];
 
 	i = 0;
 	j = 0;
 	k = left;
-	while (i < n1 && j < n2) {
-		if (left_tmp[i] <= right_tmp[j]) {
-            array[k] = left_tmp[i];
-            i++;
-        }
-        else {
+	while (i < n1 && j < n2)
+	{
+		if (left_tmp[i] <= right_tmp[j])
+		{
+			array[k] = left_tmp[i];
+			i++;
+		}
+		else
+		{
 			array[k] = right_tmp[j];
 			j++;
 		}
 		k++;
 	}
 
-    while (i < n1) {
-        array[k] = left_tmp[i];
-        i++;
-        k++;
+	while (i < n1)
+	{
+		array[k] = left_tmp[i];
+		i++;
+		k++;
 	}
-	while (j < n2) {
-        array[k] = right_tmp[j];
-        j++;
-        k++;
-    }
+	while (j < n2)
+	{
+		array[k] = right_tmp[j];
+		j++;
+		k++;
+	}
 }
