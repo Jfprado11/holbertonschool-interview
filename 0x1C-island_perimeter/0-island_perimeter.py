9fa578158a21b14b7eada36e48a03c0aa38b8c3c
Felipe Prado <jfpc11@misena.edu.co>
2022-07-13 22:23:30 -0500

    adding the solution for island perimiter

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Module fot the solution the island piramid
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of a grid"""
+    count = 0
+    lists = len(grid)
+    single = len(grid[0])
+    for y in range(lists):
+        for x in range(single):
+            if grid[y][x] == 1:
+                count += 4
+                if (y - 1) >= 0 and grid[y - 1][x] == 1:
+                    count -= 1
+                if (x - 1) >= 0 and grid[y][x - 1] == 1:
+                    count -= 1
+                if (x + 1) < single and grid[y][x + 1] == 1:
+                    count -= 1
+                if (y + 1) < lists and grid[y + 1][x] == 1:
+                    count -= 1
+
+    return count
