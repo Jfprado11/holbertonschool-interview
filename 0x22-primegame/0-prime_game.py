ed31952de8990d13990c31d7d354aa10067a3f9e
Felipe Prado <jfpc11@misena.edu.co>
2022-08-24 20:09:05 -0500

    fixing comments

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -5,9 +5,9 @@ a function to evaluate the result the prime game
 
 
 def isWinner(x, nums):
-    values = [0, 0, 1, 1, 2, 2, 3, 3, 4]
     """evaluates who is the winner a return the name of the winner person
     """
+    values = [0, 0, 1, 1, 2, 2, 3, 3, 4]
     number = 0
 
     for num in nums:
