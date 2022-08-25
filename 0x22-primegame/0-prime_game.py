5df03521f53a5a377fba307df159f15785c35027
Felipe Prado <jfpc11@misena.edu.co>
2022-08-24 20:12:02 -0500

    looking for edges cases

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -7,6 +7,9 @@ a function to evaluate the result the prime game
 def isWinner(x, nums):
     """evaluates who is the winner a return the name of the winner person
     """
+    if (x < 1):
+        return None
+
     values = [0, 0, 1, 1, 2, 2, 3, 3, 4]
     number = 0
 
